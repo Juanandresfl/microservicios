c204ca1b0cff6cda83ace32359b6c11e1dfc4eb9
Juan Fuentes <43227783+Juanandresfl@users.noreply.github.com>
2021-07-02 15:04:08 -0500

    add registry

---
+++ servicio-registry/src/test/java/com/pragma/servicioregistry/ServicioRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.servicioregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
