933973a502c17e4035abf1af7f57fbff47e3db4d
Juan Fuentes <43227783+Juanandresfl@users.noreply.github.com>
2021-07-06 14:37:08 -0500

    controller advice

--- servicicio-cliente/src/main/java/com/pragma/cliente/controllers/ErrorController.java
+++ servicicio-cliente/src/main/java/com/pragma/cliente/controllers/ErrorController.java
@@ -2,10 +2,8 @@ package com.pragma.cliente.controllers;
 
 import com.pragma.cliente.exception.ClienteException;
 import com.pragma.cliente.exception.ErrorMensaje;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
